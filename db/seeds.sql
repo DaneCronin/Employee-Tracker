e4b4fae517e02216371e2df09e0c11af8cc888dd
Dane Cronin <danecronin@gmail.com>
2022-10-04 17:39:08 -0600

    added employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,3 +18,16 @@ VALUES
         ('Salesperson', '80000', 1),
         ('Software Engineer', '120000', 2),
         ('Lawyer', '190000', 4);
+
+
+-- EMPLOYEE SEEDS --
+-- INSERT INTO employee (first_name, last_name, manager_id, role_id)
+-- VALUES 
+--     ("John", "Doe", 3, 1),
+--     ("Mike", "Chan", 1, 2),
+--     ("Ashley", "Rodriguez", null, 3),
+--     ("Kevin", "Tupik", 3, 4),
+--     ("Malia", "Brown", null, 5),
+--     ("Sarah", "Lourd", null, 2),
+--     ("Tom", "Allen", 7, 4),
+--     ("Christian", "Eckenrode", 2, 1);
